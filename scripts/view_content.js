ee7d4d8e3309f2f4b94decb734f837e0c503da37
psamusev <psamusev2608@gmail.com>
2014-06-13 11:25:27 +0300

    style fixes

--- scripts/view_content.js
+++ scripts/view_content.js
@@ -231,7 +231,7 @@ function addContactDialog(){
             '</div>' +
             '<div class="inner">' +
                 '<div>' +
-                    '<span><strong>Enter user ID(chat ID):</strong></span>' +
+                    '<span><strong>Enter user ID:</strong></span>' +
                     '<input id="vk_user_chat_id" type="number" value="1" min="1" />' +
                 '</div>' +
                 '<div id="searchContact" class="button_blue" style="top:-7px;margin-left: 10px"><button>Find</button></div>' +
